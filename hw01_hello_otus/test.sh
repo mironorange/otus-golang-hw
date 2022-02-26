03f293d03bf99deb2061f0b030308aa64faddfda
Ivan Mironov <ivan.mironov@x5.ru>
2022-02-26 18:37:08 +0300

    HW01 Checking module installation

--- hw01_hello_otus/test.sh
+++ hw01_hello_otus/test.sh
@@ -5,4 +5,12 @@ expected='!SUTO ,olleH'
 result=$(go run main.go | sed 's/^ *//;s/ *$//')
 [ "${result}" = "${expected}" ] || (echo -e "invalid output: ${result}" && exit 1)
 
+# # Checking module installation
+# # TODO: Replace branch after merge to master
+# docker run --rm -i golang:1.17-alpine sh << 'EOF'
+# go install github.com/mironorange/otus-golang-hw/hw01_hello_otus@hw01_hello_otus \
+#     && [ -d "$(go env GOMODCACHE)/github.com/mironorange/otus-golang-hw" ] \
+#         && echo "Successful"
+# EOF
+
 echo "PASS"
