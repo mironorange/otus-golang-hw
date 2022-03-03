d2e235435778372bfe2158079dbf184a43e75c74
Ivan Mironov <ivan.mironov@x5.ru>
2022-03-03 23:15:59 +0300

    HW02 Pass all test

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -16,9 +16,10 @@ func Unpack(src string) (string, error) {
 
 	sc := make([]byte, 0)
 	ml := 1
+	shielding := false // Shielding mode
 	sequences := make([]byte, 0)
 	for _, char := range src {
-		if unicode.IsDigit(char) {
+		if unicode.IsDigit(char) && !shielding {
 			if len(sc) <= 0 {
 				return "", ErrInvalidString
 			}
@@ -26,23 +27,34 @@ func Unpack(src string) (string, error) {
 			if err != nil {
 				return "", ErrInvalidString
 			}
+
 			sequences = append(sequences, bytes.Repeat(sc, ml)...)
 			ml = 1
 			sc = sc[:0]
 		} else {
+			if char == '\\' && !shielding {
+				if len(sc) > 0 {
+					sequences = append(sequences, sc...)
+					ml = 1
+					sc = sc[:0]
+				}
+
+				shielding = true
+				continue
+			}
 			if len(sc) > 0 {
 				sequences = append(sequences, sc...)
 				ml = 1
 				sc = sc[:0]
 			}
+
 			sc = append(sc, byte(char))
+			shielding = false
 		}
 	}
 
 	if len(sc) > 0 {
 		sequences = append(sequences, bytes.Repeat(sc, ml)...)
-	} else {
-		return "", ErrInvalidString
 	}
 
 	return string(sequences), nil
