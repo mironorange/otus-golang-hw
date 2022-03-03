5aa595d291f545d29dfd6dce152855576580f394
Ivan Mironov <ivan.mironov@x5.ru>
2022-03-03 22:22:37 +0300

    HW02 Made easier

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,46 +10,34 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(src string) (string, error) {
+	if len(src) <= 0 {
+		return "", nil
+	}
+
+	sc := make([]byte, 0)
+	ml := 1
 	sequences := make([]byte, 0)
-	sc := make([]byte, 0) // sequence
-	sequence := make([]byte, 0)
-	ml := make([]byte, 0) // multiplier
 	for _, char := range src {
 		if unicode.IsDigit(char) {
+			ml, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			sequences = append(sequences, bytes.Repeat(sc, ml)...)
+			ml = 1
+			sc = sc[:0]
+		} else {
 			if len(sc) > 0 {
-				sequence = sc
+				sequences = append(sequences, sc...)
+				ml = 1
 				sc = sc[:0]
 			}
-
-			ml = append(ml, byte(char))
-		} else {
-			if len(sequence) > 0 {
-				if len(ml) > 0 {
-					multiplier, err := strconv.Atoi(string(ml))
-					if err != nil {
-						return "", ErrInvalidString
-					}
-					ml = ml[:0]
-					sequences = append(sequences, bytes.Repeat(sequence, multiplier)...)
-				} else {
-					sequences = append(sequences, sequence...)
-				}
-			}
-
 			sc = append(sc, byte(char))
 		}
 	}
 
-	if len(sequence) > 0 {
-		if len(ml) > 0 {
-			multiplier, err := strconv.Atoi(string(ml))
-			if err != nil {
-				return "", ErrInvalidString
-			}
-			sequences = append(sequences, bytes.Repeat(sequence, multiplier)...)
-		} else {
-			sequences = append(sequences, sequence...)
-		}
+	if len(sc) > 0 {
+		sequences = append(sequences, bytes.Repeat(sc, ml)...)
 	} else {
 		return "", ErrInvalidString
 	}
