15d21b1d2c9db3ecba02c6f9fe47d4934e08aaaf
Ivan Mironov <ivan.mironov@x5.ru>
2022-03-03 23:35:36 +0300

    HW01 Check linter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,7 +10,7 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(src string) (string, error) {
-	if len(src) <= 0 {
+	if len(src) == 0 {
 		return "", nil
 	}
 
@@ -19,35 +19,32 @@ func Unpack(src string) (string, error) {
 	shielding := false // Shielding mode
 	sequences := make([]byte, 0)
 	for _, char := range src {
-		if unicode.IsDigit(char) && !shielding {
-			if len(sc) <= 0 {
-				return "", ErrInvalidString
-			}
-			ml, err := strconv.Atoi(string(char))
-			if err != nil {
-				return "", ErrInvalidString
-			}
-
-			sequences = append(sequences, bytes.Repeat(sc, ml)...)
-			ml = 1
-			sc = sc[:0]
-		} else {
-			if char == '\\' && !shielding {
-				if len(sc) > 0 {
-					sequences = append(sequences, sc...)
-					ml = 1
-					sc = sc[:0]
-				}
-
-				shielding = true
-				continue
-			}
+		if char == '\\' && !shielding {
 			if len(sc) > 0 {
 				sequences = append(sequences, sc...)
 				ml = 1
 				sc = sc[:0]
 			}
 
+			shielding = true
+			continue
+		}
+		if unicode.IsDigit(char) && !shielding {
+			if len(sc) == 0 {
+				return "", ErrInvalidString
+			}
+			ml, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			sequences = append(sequences, bytes.Repeat(sc, ml)...)
+			sc = sc[:0]
+		} else {
+			if len(sc) > 0 {
+				sequences = append(sequences, sc...)
+				ml = 1
+				sc = sc[:0]
+			}
 			sc = append(sc, byte(char))
 			shielding = false
 		}
