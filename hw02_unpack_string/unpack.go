8c706d4c5d35bb2bf707ee9758835542a16542d7
Ivan Mironov <ivan.mironov@x5.ru>
2022-03-07 21:06:46 +0300

    HW02 Add test

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,7 +7,14 @@ import (
 	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString = errors.New("invalid string")
+	SpecialSymbols   = map[rune]int{
+		'n': 1,
+		'r': 1,
+		't': 1,
+	}
+)
 
 func Unpack(src string) (string, error) {
 	if len(src) == 0 {
@@ -19,16 +26,7 @@ func Unpack(src string) (string, error) {
 	shielding := false // Shielding mode
 	sequences := make([]byte, 0)
 	for _, char := range src {
-		if char == '\\' && !shielding {
-			if len(sc) > 0 {
-				sequences = append(sequences, sc...)
-				ml = 1
-				sc = sc[:0]
-			}
-
-			shielding = true
-			continue
-		}
+		// Если повстречалась цифра или включен режим экранирования
 		if unicode.IsDigit(char) && !shielding {
 			if len(sc) == 0 {
 				return "", ErrInvalidString
@@ -39,15 +37,29 @@ func Unpack(src string) (string, error) {
 			}
 			sequences = append(sequences, bytes.Repeat(sc, ml)...)
 			sc = sc[:0]
-		} else {
+			continue
+		}
+		// Если встретился обратный слеш, то проигнорировать символ и включить режим экранирования
+		if char == '\\' && !shielding {
 			if len(sc) > 0 {
 				sequences = append(sequences, sc...)
 				ml = 1
 				sc = sc[:0]
 			}
-			sc = append(sc, byte(char))
-			shielding = false
+			shielding = true
+			continue
 		}
+		// В любом ином случае рассматриваем как символ, который нужно распаковать
+		if len(sc) > 0 {
+			sequences = append(sequences, sc...)
+			ml = 1
+			sc = sc[:0]
+		}
+		if _, ok := SpecialSymbols[char]; ok && shielding {
+			sc = append(sc, '\\')
+		}
+		sc = append(sc, byte(char))
+		shielding = false
 	}
 
 	if len(sc) > 0 {
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -21,6 +21,7 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `abcd`, expected: `abcd`},
 	}
 
 	for _, tc := range tests {
@@ -42,4 +43,36 @@ func TestUnpackInvalidString(t *testing.T) {
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
+}
+
+func TestEmptyStringLength(t *testing.T) {
+	input := ""
+	result, _ := Unpack(input)
+	require.Equal(t, 0, len(result))
+}
+
+func TestUnpackWithSpecialSymbols(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: `d\n5abc`, expected: `d\n\n\n\n\nabc`},
+		{input: `dn5abc`, expected: `dnnnnnabc`},
+		{input: `dn5\abc`, expected: `dnnnnnabc`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestErrorReturning(t *testing.T) {
+	input := "111"
+	_, err := Unpack(input)
+	require.Error(t, err)
 }
