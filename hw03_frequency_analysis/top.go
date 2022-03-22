072cefbc6e7a5599f9b85f850475b09569834659
Ivan Mironov <ivan.mironov@x5.ru>
2022-03-22 10:16:01 +0300

    HW03 Draft homework

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,65 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type dimension struct {
+	word  string
+	count int
+}
+
+// Change to true if needed.
+var taskWithAsteriskIsCompleted = true
+var reg = regexp.MustCompile(`\s+`)
+
+func Top10(text string) []string {
+	if len(text) <= 0 {
+		return make([]string, 0)
+	}
+
+	counter := make(map[string]int)
+
+	safe := reg.ReplaceAllString(text, " ")
+
+	for _, v := range strings.Split(safe, " ") {
+
+		if taskWithAsteriskIsCompleted {
+			if v == "-" {
+				continue
+			}
+			v = strings.Trim(v, ".")
+			v = strings.ToLower(v)
+		}
+
+		counter[v]++
+	}
+
+	ds := make([]dimension, 0)
+	for value, key := range counter {
+		d := dimension{
+			word:  value,
+			count: key,
+		}
+		ds = append(ds, d)
+	}
+
+	sort.Slice(ds, func(i, j int) bool {
+		if ds[i].count == ds[j].count {
+			return strings.Compare(ds[i].word, ds[j].word) < 0
+		}
+		return ds[i].count > ds[j].count
+	})
+
+	words := make([]string, 0)
+
+	// fmt.Println(ds)
+
+	for i := 0; i < len(ds) && i < 10; i++ {
+		words = append(words, ds[i].word)
+	}
+
+	return words
 }
