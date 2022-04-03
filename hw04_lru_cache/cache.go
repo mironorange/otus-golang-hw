fb31066650b5037138bd6c79ff31de088f36aea9
Ivan Mironov <ivan.mironov@x5.ru>
2022-04-03 18:53:43 +0300

    HW04 Add Cache methods

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,9 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
-	queue    List
-	items    map[Key]*ListItem
+	queue    DoublyLinkedLister
+	items    map[Key]*DLListItem
 }
 
 type cacheItem struct {
@@ -21,10 +19,64 @@ type cacheItem struct {
 	value interface{}
 }
 
+// Получить ранее установленное значение кеша и флаг его присутствия
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	var value interface{}
+	isExists := false
+	queueItem, ok := cache.items[key]
+	if ok {
+		isExists = true
+		item := queueItem.Value
+		value = item.(*cacheItem).value
+		cache.queue.MoveToFront(queueItem)
+	}
+	return value, isExists
+}
+
+// Установить значение кеша
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	queue := cache.queue
+	isExists := false
+	queueItem, ok := cache.items[key]
+	if ok {
+		isExists = true
+		queue.Remove(queueItem)
+	}
+	item := &cacheItem{
+		key:   key,
+		value: value,
+	}
+	// Если операция добавления элемента приведет к переполнению списка
+	// Перед тем как добавлять новый элемент необходимо
+	// Вытолкнуть последний элемент списка
+	if queue.Len() >= (cache.capacity + 1) {
+		back, err := queue.Back()
+		if err != nil {
+			queue.Remove(back)
+		}
+	}
+	queueItem = queue.PushFront(item)
+	cache.items[key] = queueItem
+	return isExists
+}
+
+// Очистить кеш
+func (cache *lruCache) Clear() {
+	item, err := cache.queue.Front()
+	if err != nil {
+		return
+	}
+	for i := item; i != nil; i = i.Next {
+		key := i.Value.(*cacheItem).key
+		delete(cache.items, key)
+	}
+}
+
+// Инициировать новое значение кеша
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		queue:    NewDoublyLinkedList(capacity),
+		items:    make(map[Key]*DLListItem, capacity),
 	}
 }
