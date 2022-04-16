9575d83ecf39ed22a2b03439d7f56fe7662db837
Ivan Mironov <ivan.mironov@x5.ru>
2022-04-16 17:59:22 +0300

    HW05 Fix race condition

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -4,10 +4,10 @@ go 1.16
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/stretchr/testify v1.7.0
-	go.uber.org/goleak v1.1.10
-	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
-	golang.org/x/tools v0.1.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/stretchr/testify v1.7.1
+	go.uber.org/goleak v1.1.12
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 	"sync/atomic"
 )
 
@@ -14,7 +15,7 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	var errorsCount, runTasksCount int32
+	var errorsCount int32
 	if len(tasks) == 0 {
 		return nil
 	}
@@ -24,50 +25,29 @@ func Run(tasks []Task, n, m int) error {
 	if n <= 0 {
 		return ErrWorkersLimited
 	}
-
+	wg := sync.WaitGroup{}
+	wg.Add(n)
 	tasksChannel := make(chan Task, len(tasks))
-	errorsChannel := make(chan struct{}, n)
-	terminationChannel := make(chan struct{}, n)
-
 	for i := 0; i < n; i++ {
 		go func() {
-			// Если не было ошибок, то горутина завершит свою работу, так как закроется канал.
+			defer wg.Done()
 			for task := range tasksChannel {
-				if err := task(); err != nil {
-					errorsChannel <- struct{}{}
-				}
-				terminationChannel <- struct{}{}
-				// Если количество ошибок больше ожидаемых, то завершить чтение из канала.
-				// И завершить работу горутины
 				if errorsCount > int32(m) {
 					break
+				}
+				if err := task(); err != nil {
+					atomic.AddInt32(&errorsCount, 1)
 				}
 			}
 		}()
 	}
-
 	for _, task := range tasks {
 		tasksChannel <- task
 	}
 	close(tasksChannel)
-
-	for {
-		// Если не добавлена секция default, то оператор select ждет сообщение от одного из канала.
-		select {
-		// Считаем количество завершившихся задач.
-		case <-terminationChannel:
-			atomic.AddInt32(&runTasksCount, 1)
-		// Считаем количество возникших ошибок.
-		case <-errorsChannel:
-			atomic.AddInt32(&errorsCount, 1)
-		// Контролируем количество завершившихся задач и количество ошибок.
-		default:
-			if runTasksCount == int32(len(tasks)) {
-				return nil
-			}
-			if errorsCount > int32(m) {
-				return ErrErrorsLimitExceeded
-			}
-		}
+	wg.Wait()
+	if errorsCount > int32(m) {
+		return ErrErrorsLimitExceeded
 	}
+	return nil
 }
