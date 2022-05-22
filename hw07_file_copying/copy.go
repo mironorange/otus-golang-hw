11cecfbc2e962631643c52bbd316f9739d7bf6f0
Ivan Mironov <ivan.mironov@x5.ru>
2022-05-22 20:15:50 +0300

    HW07 Draft homework

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,62 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Если не удалось открыть файл, то и скопировать его нельзя
+	dst, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	dstStats, _ := dst.Stat()
+
+	// Offset больше, чем размер файла - невалидная ситуация
+	if offset > dstStats.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Limit равный нулю, говорит об отсутствии ограничений
+	if limit == 0 {
+		limit = dstStats.Size()
+	}
+
+	// Если записать файл нельзя, то и операция не выполнится
+	src, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	dst.Seek(offset, io.SeekStart)
+	totalBytesCopied := 0
+
+	buf := make([]byte, 1*1024)
+	for {
+		readSize, err := dst.Read(buf)
+		if totalBytesCopied+readSize > int(limit) {
+			readSize = int(limit) - totalBytesCopied
+		}
+
+		writeSize, _ := src.Write(buf[0:readSize])
+
+		totalBytesCopied += writeSize
+		if totalBytesCopied == int(limit) {
+			break
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+
+	defer dst.Close()
+	defer src.Close()
 	return nil
+}
+
+func iOpenAndReadAllFile(dstFilename string) ([]byte, error) {
+	file, err := os.Open(dstFilename)
+	if err != nil {
+		return nil, err
+	}
+	buf, err := io.ReadAll(file)
+	file.Close()
+	return buf, err
 }
