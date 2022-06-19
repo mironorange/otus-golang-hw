830b31c9a92df375a248f9927fb9c18b46609522
Ivan Mironov <ivan.mironov@x5.ru>
2022-06-19 17:14:41 +0300

    HW11. Draft homework

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,125 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	fmt.Println("It`s work!")
+
+	//// Читать из входящего потока ввода и выводить на экран
+	//scanner := bufio.NewScanner(os.Stdin)
+	//for scanner.Scan() {
+	//	line := scanner.Text()
+	//	fmt.Println(line)
+	//}
+
+	//// Соединиться по tcp с сервером
+	//dialer := &net.Dialer{}
+	//ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//defer cancel()
+	//
+	//conn, err := dialer.DialContext(ctx, "tcp", "127.0.0.1:9000")
+	//if err != nil {
+	//	log.Fatalf("Cannot connect: %v", err)
+	//}
+	//
+	//<-ctx.Done()
+	//time.Sleep(5*time.Second)
+	//conn.Close()
+
+	//// Соединиться по tcp с сервером и записать в него данные из потока ввода
+	//dialer := &net.Dialer{}
+	//
+	//ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//
+	//conn, err := dialer.DialContext(ctx, "tcp", "127.0.0.1:9000")
+	//if err != nil {
+	//	log.Fatalf("Cannot connect: %v", err)
+	//}
+	//
+	//go readFromConnection(ctx, conn, cancel)
+	//go writeToConnection(ctx, conn, cancel)
+	//
+	//<-ctx.Done()
+	//time.Sleep(5*time.Second)
+	//conn.Close()
+
+	//// Ctrl-D sends EOF which doesn't make sense when you're not getting input.
+	//// Выйти из приложения при условии, что получен из вне сигнал о выходе
+	//c := make(chan os.Signal, 1)
+	//signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	//<-c
+	//fmt.Println("Bye!")
+
+	client := NewTelnetClient("127.0.0.1:9000", 5*time.Second, os.Stdin, os.Stdout)
+	defer client.Close()
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	if err := client.Connect(); err == nil {
+		go func() {
+			for {
+				if err := client.Send(); err != nil {
+					return
+				}
+			}
+		}()
+
+		go func() {
+			for {
+				if err := client.Receive(); err != nil {
+					return
+				}
+			}
+		}()
+	}
+
+	<-ctx.Done()
+}
+
+func readFromConnection(ctx context.Context, conn net.Conn, cancel context.CancelFunc) {
+	scanner := bufio.NewScanner(conn)
+	func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if !scanner.Scan() {
+					return
+				}
+				text := scanner.Text()
+				log.Printf("From server: %s", text)
+			}
+		}
+	}()
+	cancel()
+}
+
+func writeToConnection(ctx context.Context, conn net.Conn, cancel context.CancelFunc) {
+	scanner := bufio.NewScanner(os.Stdin)
+	func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if !scanner.Scan() {
+					return
+				}
+				line := scanner.Text()
+				log.Printf("To server %v\n", line)
+
+				conn.Write([]byte(fmt.Sprintf("%s\n", line)))
+			}
+		}
+	}()
+	cancel()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,9 +16,84 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Telnet struct {
+	ctx         context.Context
+	cancel      context.CancelFunc
+	conn        net.Conn
+	connScanner *bufio.Scanner
+	address     string
+	timeout     time.Duration
+	inScanner   *bufio.Scanner
+	outWriter   io.Writer
+}
+
+func (t *Telnet) Connect() error {
+	ctx, cancel := context.WithTimeout(context.Background(), t.timeout)
+
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(ctx, "tcp", t.address)
+	if err != nil {
+		cancel()
+		return err
+	}
+
+	t.ctx = ctx
+	t.cancel = cancel
+	t.conn = conn
+	t.connScanner = bufio.NewScanner(conn)
 	return nil
+}
+
+func (t *Telnet) Close() error {
+	t.cancel()
+	if err := t.conn.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Telnet) Send() error {
+	select {
+	case <-t.ctx.Done():
+		return nil
+	default:
+		if !t.inScanner.Scan() {
+			return nil
+		}
+		line := t.inScanner.Text()
+		_, err := t.conn.Write([]byte(fmt.Sprintf("%s\n", line)))
+		if err != nil {
+			t.cancel()
+		}
+		return err
+	}
+}
+
+func (t *Telnet) Receive() error {
+	select {
+	case <-t.ctx.Done():
+		return nil
+	default:
+		if !t.connScanner.Scan() {
+			return nil
+		}
+		line := t.connScanner.Text()
+		_, err := t.outWriter.Write([]byte(fmt.Sprintf("%s\n", line)))
+		if err != nil {
+			t.cancel()
+		}
+		return err
+	}
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	// Не могу создать соединение на этом этапе, так как физическое соединение и ошибка происходит в функции Connect()
+	return &Telnet{
+		address:   address,
+		timeout:   timeout,
+		inScanner: bufio.NewScanner(in),
+		outWriter: out,
+	}
 }
 
 // Place your code here.
